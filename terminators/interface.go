c9272d78a855e9b72746fc00de131eb9303fc478
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-11-29 23:00:00 +0300

    Add the comment for the terminators.SearchTerminator interface

--- terminators/interface.go
+++ terminators/interface.go
@@ -3,5 +3,7 @@ package terminators
 // SearchTerminator ...
 type SearchTerminator interface {
 	IsSearchTerminated(deep int) bool
+
+	// It should return a value between 0 and 1 inclusive.
 	SearchProgress(deep int) float64
 }
